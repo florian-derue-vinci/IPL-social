5323195aabe726f87be431e53d9ef5da07cdeed5
SpaceArty <evoli1512@gmail.com>
2026-01-16 16:37:21 +0100

    feat: implémentation de la vérification de point dans le domaine de l'email  avec tests

--- src/validationEmail.ts
+++ src/validationEmail.ts
@@ -1,4 +1,3 @@
-
 /*
     Valide un email selon certains critères
     email : string | l'email a valider
@@ -8,5 +7,11 @@ export function validateEmail(email: string) : boolean{
     // Doit contenir au moins 1 @
     if (!email.includes("@")) return false;
 
+    // Séparer local (ce qu'il y a avant le @) et domaine (ce qu'il y a après)
+    const parts = email.split("@");
+    const domaine = parts[parts.length -1];
+
+    // Doit contenir au moins 1 point dans le domaine (et pas en dernier charactère)
+    if (!domaine.includes(".") || domaine.endsWith(".")) return false;
     return true;
 }
--- tests/validationEmail.test.ts
+++ tests/validationEmail.test.ts
@@ -2,7 +2,7 @@ import { validateEmail } from "../src/validationEmail"
 
 describe("validateEmail", () => {
 
-    describe("Contient au moins 1 @", () => {
+    describe("Il doit contenir au moins un @", () => {
         it("devrait rejeter un email qui ne contient pas de @", () => {
             //Arrange
             const email = "test123.gmail"
@@ -12,7 +12,33 @@ describe("validateEmail", () => {
 
             //Assert
             expect(result).toBe(false);
-        })
+        });
 
-    })
+    });
+
+    describe("Il doit contenir au moins un point dans le nom de domaine (et pas le dernier caractère)", () => {
+
+        it("devrait rejeter un email qui ne contient pas de point", () => {
+            //Arrange
+            const email = "test123gmail"
+            
+            //Act
+            const result = validateEmail(email);
+
+            //Assert
+            expect(result).toBe(false);
+        });
+
+        it("devrait rejeter un email qui contient un point en dernier charactère", () => {
+            //Arrange
+            const email = "test123gmail."
+            
+            //Act
+            const result = validateEmail(email);
+
+            //Assert
+            expect(result).toBe(false);
+        });
+
+    });
 })
